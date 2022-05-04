845b35f9b96a01ccfe4e22beec2c6f19b2472baa
Helene <uniruusu@gmail.com>
2022-05-04 15:37:47 +0300

    First and Second exercise

---
+++ Excercise2/Excercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            // Program that calculates area for a room from user input. Used for calculating price for carpeting with constant price of carpet per square meter.
+
+            int CostofCarpet = 5;
+            Console.WriteLine("Give length of the room");
+            int Length = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give width of the room");
+            int Width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Calculating costs");
+
+            int area = Length * Width;
+
+            int Price = area * CostofCarpet;
+
+            Console.WriteLine("Your room was size of {0} square meters. Cost of the carpet is {1} Euros. The total cost of carpeting the room comes to {2} Euros ", area, CostofCarpet, Price);
+        }
+    }
+}
---
+++ exercise1/exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercise1
+{
+    class Program
+    {
+        // simple calculator, user input integers and adding them together
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input first integer");
+            int Firstinput = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please input second integer");
+            int Secondinput = int.Parse(Console.ReadLine());
+
+            int answer = Firstinput + Secondinput;
+
+            Console.WriteLine("calculating following");
+            Console.WriteLine("{0} + {1} = {2}", Firstinput, Secondinput, answer);
+
+        }
+    }
+
+}
