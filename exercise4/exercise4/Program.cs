cb433cae7a656d0ffb9e31186dcc8a936ca64d31
Helene <uniruusu@gmail.com>
2022-05-04 16:03:00 +0300

    Fourth exercise

---
+++ exercise4/exercise4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program takes hourly pay rate from a user. It will prompt error if the pay rate is lower than 7.50€ or higher than 49.00€. Otherwise program indicates that the rate is okay
+
+            Console.WriteLine("Give pay rate:");
+            double payrate = double.Parse(Console.ReadLine());
+
+            if (payrate < 7.50 || payrate > 49.00)
+            {
+                Console.WriteLine("Invalid rate");
+            }
+            else
+            {
+                Console.WriteLine("Payrate is valid");
+            }
+
+
+        }
+
+    }
+}
