c9c2c4afd3a66dea34487b0f4ea3c9aefbbe3390
Helene <uniruusu@gmail.com>
2022-05-04 17:07:30 +0300

    Eight exercise

---
+++ exercise8/exercise8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program sums integers from 1 to 50
+
+            int sum = 0;
+            for(int i = 1; i <= 50; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("The sum is {0}",sum);
+        }
+    }
+}
