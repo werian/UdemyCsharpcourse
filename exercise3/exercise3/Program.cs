309003622c7d116816be9fc10b636f3f70339ee0
Helene <uniruusu@gmail.com>
2022-05-04 15:49:53 +0300

    Third exercise

---
+++ exercise3/exercise3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program that asks user for total amount of time in minutes
+            // Program then returns the amount of time in hours and minutes
+
+            Console.WriteLine("Give amount of time in minutes");
+            int TotalTimeinMinutes = int.Parse(Console.ReadLine());
+
+
+            int Hours = TotalTimeinMinutes / 60;
+
+            int RemainingMinutes = TotalTimeinMinutes % 60;
+
+            Console.WriteLine("The amount of time in minutes was {0} which is equal to {1} hours and {2} minutes ", TotalTimeinMinutes, Hours, RemainingMinutes);
+        }
+    }
+}
