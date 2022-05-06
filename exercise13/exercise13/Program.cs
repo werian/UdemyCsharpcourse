4e4bf57540d399ac0f1884fd980367842a85a07d
Helene <uniruusu@gmail.com>
2022-05-06 16:07:01 +0300

    13th exercise

---
+++ exercise13/exercise13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise13
+{
+    class Program
+    {
+        //Program asks user for the interger input, after which it calls method to create multiplication table
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+            MultiplicationTable(number);
+        }
+
+        public static void MultiplicationTable(int number)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                int answer = number * i;
+                Console.WriteLine("{0,2} * {1,2} = {2,2}",number,i,answer);
+            }
+        }
+    }
+}
