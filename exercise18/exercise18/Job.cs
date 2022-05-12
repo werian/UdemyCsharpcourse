dfff3174cb91ff13bbf75e9a62decd7590f8c0a8
Helene <uniruusu@gmail.com>
2022-05-12 17:57:21 +0300

    18th and final exercise

---
+++ exercise18/exercise18/Job.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise18
+{
+    class Job
+    {
+        private string jobDesc;
+        private double timeToComplete;
+        private double hourlyRate;
+        private double totalFee;
+
+
+        public Job(string desc, double timeToComplete, double hourlyRate)
+        {
+            JobDesc = desc;
+            TimeToComplete = timeToComplete;
+            HourlyRate = hourlyRate;
+        }
+
+        public string JobDesc
+        {
+            get { return jobDesc; }
+            set { jobDesc = value; }
+        }
+        public double TimeToComplete
+        {
+            get { return timeToComplete; }
+            set
+            {
+                timeToComplete = value;
+                calcTotalFee();
+            }
+        }
+
+        public double HourlyRate
+        {
+            get { return hourlyRate; }
+            set
+            {
+                hourlyRate = value;
+                calcTotalFee();
+            }
+        }
+       
+        public double TotalFee
+        {
+            get { return totalFee; }
+        }
+
+       
+
+        public static Job operator +(Job j1, Job j2)
+        {
+            string newDesc = j1.JobDesc + " and " + j2.JobDesc;
+            double newTime = j1.TimeToComplete + j2.TimeToComplete;
+            double newHourlyRate = (j1.HourlyRate + j2.HourlyRate) / 2;
+            Job addedJob = new Job(newDesc, newTime, newHourlyRate);
+            return addedJob;
+        }
+
+        private void calcTotalFee()
+        {
+             totalFee = hourlyRate * timeToComplete;
+        }
+    }
+}
+
---
+++ exercise18/exercise18/Program.cs
@@ -0,0 +1,26 @@
+﻿using exercise18;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Job j1 = new Job("Wash windows", 5, 15);
+            Console.WriteLine("{0}, {1}, {2}, {3}",j1.JobDesc, j1.TimeToComplete, j1.HourlyRate, j1.TotalFee);
+            Job j2 = new Job("Take out trash", 6, 10.5);
+            Console.WriteLine("{0}, {1}, {2}, {3}", j2.JobDesc, j2.TimeToComplete, j2.HourlyRate, j2.TotalFee);
+            
+            Job j3 = j1 + j2;
+            Console.WriteLine("{0}, {1}, {2}, {3}", j3.JobDesc, j3.TimeToComplete, j3.HourlyRate, j3.TotalFee);
+        }
+
+
+
+    }
+}
