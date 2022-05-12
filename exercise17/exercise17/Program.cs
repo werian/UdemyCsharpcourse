b6e5e6ad0d4c46b9f7e81a8ad200a06d04d06f22
Helene <uniruusu@gmail.com>
2022-05-12 16:10:18 +0300

    17th exercise

---
+++ exercise17/exercise17/Program.cs
@@ -0,0 +1,36 @@
+﻿using exercise17;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise17
+{
+    class Program
+    {
+        /* program to uses class named Taxpayer. This class contains data fields for objects such as social security number, yearly gross income and tax owed. It also includes properties with get set
+         * and read-only. Class will be used to calculate tax.
+         * Main program will create 10 Taxpayer objects in an array.
+         * 
+        */
+        static void Main(string[] args)
+        {
+            Taxpayer[] taxpayers = new Taxpayer[10];
+
+            for (int i = 0; i < taxpayers.Length; i++)
+            {
+                taxpayers[i] = new Taxpayer();
+                Console.WriteLine(" Enter payer #{0} SSN", i+1);
+                taxpayers[i].SocialSecurityNum = Console.ReadLine();
+                Console.WriteLine("Enter payer #{0} yearly income", i + 1);
+                taxpayers[i].GrossIncome = double.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i < taxpayers.Length; i++)
+            {
+                Console.WriteLine("Tax payer #{0}, SSN = {1}, Income = {2}, Tax owed = {3}", i+1, taxpayers[i].SocialSecurityNum, taxpayers[i].GrossIncome, taxpayers[i].TaxOwed);
+            }
+        }
+    }
+}
---
+++ exercise17/exercise17/Taxpayer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise17
+{
+    class Taxpayer
+    {
+        // objects
+        private string socialSecurityNum;
+        private double grossIncome;
+        private double taxOwed;
+
+        // properties
+        public string SocialSecurityNum
+        {
+            get { return socialSecurityNum; }
+            set { socialSecurityNum = value;}
+        }
+        public double GrossIncome
+        {
+            get { return grossIncome; }
+            set
+            {
+                grossIncome = value;
+                calcTaxOwed();
+            }
+        }
+
+        public double TaxOwed
+        {
+            get { return taxOwed; }
+
+        }
+
+        //method
+        private void calcTaxOwed()
+        {
+            if (grossIncome < 30000)
+            {
+                taxOwed = grossIncome * 0.15;
+            }
+            else
+            {
+                taxOwed = grossIncome * 0.28;
+            }
+        }
+
+
+    }
+}
