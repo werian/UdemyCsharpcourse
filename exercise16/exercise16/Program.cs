557e44b3cd117f57cf4df3d4e3c318ff97394aa7
Helene <uniruusu@gmail.com>
2022-05-12 14:52:58 +0300

    16th exercise

---
+++ exercise16/exercise16/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using exercise16;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise16
+{
+    class Program
+    {
+        //Program uses class name square to contain fields for are and and length of the side.
+        static void Main(string[] args)
+        {
+            Square[] squares = new Square[10];
+
+            for (int i = 1; i < 10; i++)
+            {
+                squares[i] = new Square(i+1);
+                Console.WriteLine("{0,2}, {1,2}",squares[i].Side, squares[i].Area);
+            
+            }
+
+
+        }
+    }
+}
---
+++ exercise16/exercise16/Square.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise16
+{
+    class Square
+    {
+
+        private int side;
+        private int area;
+
+        //constractor for the side
+        public Square(int side)
+        {
+            this.side = side;
+            calcArea();
+        }
+
+        // private method that calculates the area of the square
+        private void calcArea()
+        {
+            area = side * side;
+        }
+
+        //read-only properties for the side and area
+        public int Side
+        {
+            get { return side; }
+        }
+        public int Area
+        {
+            get { return area; }
+        }
+    }
+}
